7b00afcbca70b40ae3bbb29fed689072a3c947df
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-09-29 10:56:28 +0900

    <docs> gdi+라이브러리 헤더 추가

--- framework.h
+++ framework.h
@@ -1,9 +1,12 @@
 ﻿#pragma once
 
 #include "targetver.h"
-//#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+//#define WIN32_LEAN_AND_MEAN       // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다. (GDI+를 사용하기 위해 제거)
 // Windows 헤더 파일
 #include <windows.h>
+#include <gdiplus.h> // GDI+ 헤더 파일
+#pragma comment (lib, "Gdiplus.lib") // Gdiplus.lib를 링커에 추가(정적 라이브러리)
+//using namespace Gdiplus;
 // C 런타임 헤더 파일입니다.
 #include <stdlib.h>
 #include <malloc.h>
